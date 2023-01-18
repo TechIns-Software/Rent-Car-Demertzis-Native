72bc764a32241320ab84f8eafd3e60ba74c2a48a
Nick Spanos <nickspanosf1@gmail.com>
2023-01-18 18:37:49 +0200

    basic Navigation Structure

--- App.js
+++ App.js
@@ -1,12 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import  {NavigationContainer} from  '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+function HomeScreen() {
+  return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+      <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === 'Αρχική') {
+                iconName = focused
+                    ? 'ios-information-circle'
+                    : 'ios-information-circle-outline';
+              } else if (route.name === 'Δημιουργία Αίτησης') {
+                iconName = focused ? 'ios-list' : 'ios-list-outline';
+              }else  if (route.name === 'Πρόσφατες Αιτήσεις'){
+                iconName = focused ? 'ios-folder' : 'ios-folder-outline';
+              }
+
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+          })}>
+        <Tab.Screen name="Αρχική" component={HomeScreen} />
+        <Tab.Screen name="Δημιουργία Αίτησης" component={SettingsScreen}   />
+        <Tab.Screen name="Πρόσφατες Αιτήσεις" component={SettingsScreen} options={{ tabBarBadge: 3 }} />
+      </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
   );
 }
 
