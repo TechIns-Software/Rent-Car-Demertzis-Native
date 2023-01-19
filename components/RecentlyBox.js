9e959ab80a93fa8244291a56b3dda3ad197edcf3
Nick Spanos <nickspanosf1@gmail.com>
2023-01-19 16:35:52 +0200

    box creation for Recently Applications

--- components/RecentlyBox.js
+++ components/RecentlyBox.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 function RecentlyBox(props){
-    return <View>
-        <Text>Δημιουργία Αίτησης </Text>
+    return <View style={styles.container}>
+        <View style={styles.infoContainer} >
+            <Text>Ασφάλεια {props.type}</Text>
+            <Text>Ημερομηνία: {props.date}</Text>
+        </View>
 
+        <View style={styles.isSentContainer}>
+            <Text>Εχει σταλθεί ? {props.isSent ? <Ionicons name={'ios-information-circle'} size={15}  color={'green'} />
+                : <Ionicons name={'ios-flag'} size={15}  color={'red'} />}</Text>
+        </View>
     </View>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection :'column',
+        alignContent :'space-between',
+        backgroundColor: '#bbea9c',
+       border : 'solid',
+        borderWidth : 1,
+        borderColor :'gray',
+        marginVertical :10,
+        paddingVertical :5,
+        borderRadius :10,
+        width : '90%'
+
+    },
+    infoContainer : {
+
+
+    },
+    isSentContainer: {
+
+    },
+});
 
 export default RecentlyBox;
--- screens/RecentlyApplications.js
+++ screens/RecentlyApplications.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import RecentlyBox from '../components/RecentlyBox';
 function RecentlyApplications(){
-    return <View>
-        <Text>Πρόσφατες Αιτήσεις </Text>
+    return <View style={styles.container} >
+        <RecentlyBox type={'Αυτοκινήτου'} date={'01-05-2023'} isSent={true}/>
+
+        <RecentlyBox type={'Μηχανής'} date={'01-15-2023'} isSent={false}/>
+
+        <RecentlyBox type={'Μηχανής'} date={'01-05-2023'} isSent={true}/>
 
     </View>
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+    },
+});
 
 export default RecentlyApplications;
