a419bb14a3e6f71df4bfb57017ad0db849265658
Nick Spanos <nickspanosf1@gmail.com>
2023-03-13 16:39:21 +0200

    change the format of date

--- components/form/DatePicker2.js
+++ components/form/DatePicker2.js
@@ -40,7 +40,7 @@ const DatePicker2 = ({style,label,type,customOnChange,objectKey}) => {
     };
 
     const setFormatDate = (date)=>{
-     const  formatedDate =  (date.getFullYear()+'-'+String(date.getMonth() + 1).padStart(2, '0')+'-'+String(date.getDate() ).padStart(2, '0')).toString() ;
+     const  formatedDate =  (String(date.getDate() ).padStart(2, '0')+'-'+String(date.getMonth() + 1).padStart(2, '0')+'-'+date.getFullYear()).toString() ;
         return formatedDate;
     }
 
