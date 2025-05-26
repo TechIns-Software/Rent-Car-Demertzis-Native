3bfb84503b2956378e329a93c03f1d15f9b87eb3
Mac <mac@Macs-Mac-mini.local>
2025-05-26 22:45:47 +0530

    fixed build issuse

--- components/form/input.js
+++ components/form/input.js
@@ -1,48 +1,73 @@
-import {Text, TextInput, View, StyleSheet, KeyboardAvoidingView, Platform} from "react-native";
-import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import {
+    KeyboardAvoidingView,
+    Platform,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+} from "react-native";
+import { RFPercentage } from "react-native-responsive-fontsize";
 
-function Input({label,style,TextInputConfig,name,onChangeText,inputStyle,onSubmit,editable=true,value}){
+function Input({
+  label,
+  style,
+  TextInputConfig,
+  name,
+  onChangeText,
+  inputStyle,
+  onSubmit,
+  editable = true,
+  value,
+}) {
+  const inputStyles = [styles.input];
 
-    const inputStyles = [styles.input];
+  if (TextInputConfig && TextInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
 
-    if (TextInputConfig && TextInputConfig.multiline ){
-        inputStyles.push(styles.inputMultiline);
-    }
-
-    return     <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={styles.container}>
-    <View style={[styles.inputContainer,style]}>
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <View style={[styles.inputContainer, style]}>
         <Text style={styles.label}>{label}</Text>
-        <TextInput value={value} style={[inputStyles,inputStyle]} editable={editable} name={name} onChangeText={onChangeText} {...TextInputConfig}  />
-    </View>
+        <TextInput
+          value={value}
+          style={[inputStyles, inputStyle]}
+          editable={editable}
+          name={name}
+          onChangeText={onChangeText}
+          {...TextInputConfig}
+        />
+      </View>
     </KeyboardAvoidingView>
+  );
 }
-const  styles = StyleSheet.create({
-    inputContainer :{
-        marginHorizontal :4,
-        marginVertical : 16,
-
-    },
-    label :{
-        fontSize: RFPercentage(1.4),
-        color:'#000000',
-        fontWeight :'bold',
-        marginBottom :4
-    },
-    input :{
-        backgroundColor : '#a9cafa',
-        padding :6,
-        borderRadius :6,
-        fontSize :18,
-        color :'black'
-    },
-    inputMultiline:{
-        minHeight :100,
-        textAlignVertical :'top'
-    },
-    container: {
-        flex: 1,
-    },
-})
-export default Input
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 16,
+  },
+  label: {
+    fontSize: RFPercentage(1.4),
+    color: "#000000",
+    fontWeight: "bold",
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: "#a9cafa",
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+    color: "black",
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+  container: {
+    flex: 1,
+  },
+});
+export default Input;
